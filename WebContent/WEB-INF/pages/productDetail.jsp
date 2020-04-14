3188a612fcf301ee9150e88de6b059c0965bf1e1
Allen Houng <63278069+s890257s@users.noreply.github.com>
2020-04-14 18:34:01 +0800

    productDetail
    初步商品細節

---
+++ WebContent/WEB-INF/pages/productDetail.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<style>
+.barSpace {
+	width: 100%;
+	height: 100px;
+	padding: 5px;
+}
+</style>
+
+</head>
+<body>
+<div class="barSpace"></div>
+
+
+</body>
+</html>
--- src/com/gamebase/tradesystem/controller/ProductController.java
+++ src/com/gamebase/tradesystem/controller/ProductController.java
@@ -3,14 +3,13 @@ package com.gamebase.tradesystem.controller;
 import java.io.PrintStream;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.gamebase.tradesystem.model.Product;
 import com.gamebase.tradesystem.model.service.ProductService;
-import com.google.gson.Gson;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -27,35 +26,39 @@ public class ProductController {
 	@RequestMapping(path = "/tradesystem/add", method = RequestMethod.POST)
 	@ResponseBody
 	public JSONObject tsAdd(@RequestParam(value = "form") String form) {
-		//System.out.println(productService.add(form));
+		// System.out.println(productService.add(form));
 		return productService.add(form);
 	}
+
 	@RequestMapping(path = "/tradesystem/query", method = RequestMethod.POST)
 	@ResponseBody
 	public JSONArray tsQuery() {
 		System.out.println("tsQuery");
 		return productService.query();
 	}
+
 	@RequestMapping(path = "/tradesystem/delete", method = RequestMethod.POST)
 	@ResponseBody
 	public JSONObject tsDelete(@RequestParam(value = "d") String d) {
 		return productService.delete(Integer.valueOf(d));
 	}
+
 	@RequestMapping(path = "/tradesystem/update", method = RequestMethod.POST)
 	@ResponseBody
 	public JSONObject tsUpdate(@RequestParam(value = "b") String b) {
 		return productService.update(b);
 	}
-	
+
 	@RequestMapping(path = "/tradesystem/search", method = RequestMethod.GET)
 	@ResponseBody
-	public JSONArray search(@RequestParam(value="sh") String a) {
+	public JSONArray search(@RequestParam(value = "sh") String a) {
 		System.out.println(a);
 		return productService.search(a);
 	}
+
 	@RequestMapping(path = "/tradesystem/getSearch", method = RequestMethod.POST)
 	@ResponseBody
-	public JSONArray getSearch(@RequestParam(value="search") String a) {
+	public JSONArray getSearch(@RequestParam(value = "search") String a) {
 		System.out.println(a);
 		return productService.getSearch(a);
 	}
@@ -70,5 +73,13 @@ public class ProductController {
 	public String showMainPage() {
 		System.out.println("3");
 		return "mainProductPage";
+	}
+
+	@RequestMapping(path = "/productDetail", method = RequestMethod.GET)
+	public String productDetail(@RequestParam("prodId") String prodId, Model model) {
+
+		System.out.println("id:" + prodId);
+
+		return "topBar";
 	}
 }
