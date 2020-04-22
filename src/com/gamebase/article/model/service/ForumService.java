24f0cb7ba55ec30f6ce8dab529bed02623d2c29c
Li <a0970745231@gmail.com>
2020-04-23 02:02:46 +0800

    article add controller method
    like/unlike counter

--- src/com/gamebase/article/model/service/ForumService.java
+++ src/com/gamebase/article/model/service/ForumService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gamebase.article.model.Forum;
 import com.gamebase.article.model.ForumListView;
@@ -12,7 +13,8 @@ import com.gamebase.article.model.dao.ForumDAO;
 import com.gamebase.article.model.dao.vForumListViewDAO;
 
 @Service
-public class ForumService implements IForumService {
+@Transactional
+public class ForumService {
 
 	@Autowired
 	private ForumDAO forumDao;
@@ -21,37 +23,32 @@ public class ForumService implements IForumService {
 	@Autowired
 	private vForumListViewDAO flvDao;
 
-	@Override
 	public Forum insertForum(Forum forum) {
 		Forum forumBean = forumDao.insertForum(forum);
 		return forumBean;
 	}
 
-	@Override
 	public Forum queryOneForum(Forum forum) {
 		return forumDao.queryOneForum(forum);
 	}
 
-	@Override
 	public List<Forum> queryAllForum() {
 		return forumDao.queryAllForum();
 	}
 
-	@Override
 	public Forum updateOneForum(Forum forum) {
 		return forumDao.updateOneForum(forum);
 	}
 
-	@Override
 	public boolean deleteOneForum(Forum forum) {
 		return forumDao.deleteOneForum(forum);
 	}
 
-	public List<ForumListView> queryForumListByClickNum(Integer clickRN){
+	public List<ForumListView> queryForumListByClickNum(Integer clickRN) {
 		return flvDao.queryForumListByClickNum(clickRN);
 	}
-	
-	public List<ForumListView> queryForumListByLikeNum(Integer likeRN){
+
+	public List<ForumListView> queryForumListByLikeNum(Integer likeRN) {
 		return flvDao.queryForumListByLikeNum(likeRN);
 	}
 
